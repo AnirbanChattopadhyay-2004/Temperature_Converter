b82e636584ae0e41d473a9d51bb2ee1d27117c5b
AnirbanChattopadhyay-2004 <124513264+AnirbanChattopadhyay-2004@users.noreply.github.com>
2023-12-29 00:59:23 +0530

    Update viewer.jsx

--- src/components/viewer.jsx
+++ src/components/viewer.jsx
@@ -2,7 +2,7 @@ import {Box,Typography} from "@mui/material"
 export default function Viewer({unit,degree}){
     return (
         <Box sx={{height:'100%',display:'flex',justifyContent:'center',alignItems:'center',backgroundColor:'	#088F8F'}}>
-        {degree && <Typography variant="h1" sx={{color:'aliceblue'}}>{degree}<sup>°{unit.toUpperCase()}</sup></Typography>}
+        {degree && <Typography  sx={{color:'aliceblue',typography:{xs:'h3',sm:'h3',md:'h1'}}}>{degree}<sup>°{unit.toUpperCase()}</sup></Typography>}
         </Box>
     );
 }
